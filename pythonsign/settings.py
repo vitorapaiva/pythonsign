c5f95b1d04aa89faa80c9775867d8fa2ae1e91e4
Vitor Paiva <vitorapaiva@gmail.com>
2020-07-23 15:55:52 -0300

    Update settings.py

--- pythonsign/settings.py
+++ pythonsign/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*c5x=8&f6yl&q=nt-pnf-p&ag0jn5yw(=g1qs%!@z5tfspd9e*'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
